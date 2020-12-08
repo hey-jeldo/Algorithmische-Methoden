8065d1e5a6054a6ae81d9c57004c4fe50c6bca5d
Jeldo Meppen <jeldo@hey.com>
2020-12-08 16:45:36 +0100

    complete transtition to rmd structure

--- Beispiele/kochen.py
+++
@@ -1,5 +0,0 @@
-def kochen(gericht, **zutaten):
-    zutaten['Gericht'] = gericht
-    return zutaten
-x = kochen('pizza', Mehl='300 Gramm', Olivenöl='50 ml', Tomaten='4 Stück')
-print(x)
--- Beispiele/werte.py
+++
@@ -1,5 +0,0 @@
-def rechteck(x, y):
-     return x * y
-print(rechteck(3, 4))
-werte = {'x': 3, 'y': 4}
-print(rechteck(**werte))
